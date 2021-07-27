955e2c7baacd1d592fe78f886ed2f00918795100
Chris Barbour <chris.barbour03@gmail.com>
2021-07-27 15:31:40 +0100

    including multi params

--- src/lambda-invoker.ts
+++ src/lambda-invoker.ts
@@ -3,8 +3,8 @@ import {Lambda} from 'aws-sdk';
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
 
 export function lambdaApiInvoker(arn: string, lambda: Lambda, transform: (request: APIGatewayProxyEvent) => APIGatewayProxyEvent = r => r): Caller {
-  return { call: (async (method, resource, path, body, pathParameters, queryParameters, headers) => {
-      const request = transform({ httpMethod: method, resource, path, body: body ?? null, pathParameters: pathParameters as any, headers: headers as any, queryStringParameters: queryParameters} as APIGatewayProxyEvent);
+  return { call: (async (method, resource, path, body, pathParameters, queryParameters, multiQueryParameters, headers) => {
+      const request = transform({ httpMethod: method, resource, path, body: body ?? null, pathParameters: pathParameters as any, headers: headers as any, queryStringParameters: queryParameters, multiValueQueryStringParameters: multiQueryParameters} as APIGatewayProxyEvent);
       const result = await lambda.invoke({FunctionName: arn, InvocationType: 'RequestResponse', Payload: JSON.stringify(request)}).promise();
       if(result.StatusCode! >= 200 && result.StatusCode! < 300) {
         const response: APIGatewayProxyResult = JSON.parse(result.Payload!.toString());
