9435f072d260183710d5036e7b565563fe642ca3
Chris Barbour <chris.barbour03@gmail.com>
2021-09-10 10:17:49 +0100

    added wrapper to transform callers

--- src/caller.ts
+++ src/caller.ts
@@ -11,3 +11,34 @@ export type Caller = {
     uri?: string
   ): Promise<{ statusCode: number; body: string; headers: Record<string, string> }>;
 }
+
+export type CallerArgs = {
+  method: string;
+  resource: string;
+  path: string;
+  body?: string;
+  pathParameters: Record<string, string>;
+  queryParameters: Record<string, string>;
+  multiQueryParameters: Record<string, string[]>;
+  headers: Record<string, string>;
+  uri?: string;
+}
+
+export function wrap(caller: Caller, update: (args: CallerArgs) => Partial<CallerArgs>): Caller {
+  return {
+    call: (method, resource, path, body, pathParameters, queryParameters, multiQueryParameters, headers, uri) => {
+      const updates = update({method, resource, path, body, pathParameters, queryParameters, multiQueryParameters, headers, uri});
+      return caller.call(
+        updates.method ?? method,
+        updates.resource ?? resource,
+        updates.path ?? path,
+        updates.body ?? body,
+        updates.pathParameters ?? pathParameters,
+        updates.queryParameters ?? queryParameters,
+        updates.multiQueryParameters ?? multiQueryParameters,
+        updates.headers ?? headers,
+        updates.uri ?? uri
+      );
+    }
+  }
+}
